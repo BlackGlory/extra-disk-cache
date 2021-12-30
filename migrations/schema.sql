81bd49002b0bbbe669cf67667b5929aaa6bd64fc
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-12-30 09:38:10 +0000

    chore: add schema.sql

---
+++ migrations/schema.sql
@@ -0,0 +1,13 @@
+-- 在WAL模式下, better-sqlite3可充分发挥性能
+PRAGMA journal_mode = WAL;
+
+CREATE TABLE new_cache_metadata (
+  key                  TEXT    NOT NULL UNIQUE
+, updated_at           INTEGER NOT NULL
+, time_to_live         INTEGER NOT NULL
+, time_before_deletion INTEGER NULL
+) STRICT;
+
+CREATE INDEX idx_cache_metadata_deletion_time
+          ON cache_metadata(updated_at + time_to_live + time_before_deletion)
+       WHERE time_before_deletion IS NOT NULL;
