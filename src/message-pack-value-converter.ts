8e316155905ccfae46ce089ff787ac0b8ef74a92
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-11-27 17:39:34 +0000

    refactor: add options

--- src/message-pack-value-converter.ts
+++ src/message-pack-value-converter.ts
@@ -1,12 +1,17 @@
-import { pack, unpack } from 'msgpackr'
+import { Packr } from 'msgpackr'
 import { IValueConverter } from './disk-cache-view'
 
 export class MessagePackValueConverter<T> implements IValueConverter<T> {
+  private packer = new Packr({
+    // https://github.com/kriszyp/msgpackr/issues/34
+    variableMapSize: true
+  })
+
   fromBuffer(buffer: Buffer): T {
-    return unpack(buffer)
+    return this.packer.unpack(buffer)
   }
 
   toBuffer(value: T): Buffer {
-    return pack(value)
+    return this.packer.pack(value)
   }
 }
