50d4d4fce7717b9ccb54a85118fe620a3aa23526
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-11-28 02:34:06 +0000

    docs: add JSDOC

--- src/message-pack-value-converter.ts
+++ src/message-pack-value-converter.ts
@@ -1,6 +1,12 @@
 import * as msgpack from 'msgpack-lite'
 import { IValueConverter } from './disk-cache-view'
 
+/**
+ * @deprecated
+ * There is no fast and correct implementation of MessagePack in JavaScript ecosystem.
+ * Currently using the package `msgpack-lite` to instead of the incorrect `msgpackr`,
+ * which is much slower than JSON.
+ */
 export class MessagePackValueConverter<T> implements IValueConverter<T> {
   fromBuffer(buffer: Buffer): T {
     return msgpack.decode(buffer)
