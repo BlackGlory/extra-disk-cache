d20f8fe240e2bcc989cc661c67f57e42fd506a59
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-12-06 08:41:59 +0000

    perf: fix benches

--- benches/index.ts
+++ benches/index.ts
@@ -12,42 +12,115 @@ import {
 , IndexKeyConverter as StoreIndexKeyConverter
 , JSONValueConverter as StoreJSONValueConverter
 } from 'extra-disk-store'
+import { createTempFile, remove } from 'extra-filesystem'
 
 const benchmark = new Benchmark('I/O performance')
 
 go(async () => {
   benchmark.addCase('ExtraDiskCache (write)', async () => {
-    const cache = await DiskCache.create()
+    const filename = await createTempFile()
+    const cache = await DiskCache.create(filename)
     const view = new DiskCacheView(
       cache
     , new CacheIndexKeyConverter()
     , new CacheJSONValueConverter()
     )
 
-    return () => {
-      for (let i = 1000; i--;) {
-        view.set(i, i)
+    return {
+      beforeEach() {
+        cache.clear()
+      }
+    , iterate() {
+        for (let i = 100; i--;) {
+          view.set(i, i)
+        }
+      }
+    , async afterAll() {
+        cache.close()
+        await remove(filename)
       }
     }
   })
 
   benchmark.addCase('ExtraDiskStore (write)', async () => {
-    const store = await DiskStore.create()
+    const filename = await createTempFile()
+    const store = await DiskStore.create(filename)
     const view = new DiskStoreView(
       store
     , new StoreIndexKeyConverter()
     , new StoreJSONValueConverter()
     )
 
-    return () => {
-      for (let i = 1000; i--;) {
-        view.set(i, i)
+    return {
+      beforeEach() {
+        store.clear()
+      }
+    , iterate() {
+        for (let i = 100; i--;) {
+          view.set(i, i)
+        }
+      }
+    , async afterAll() {
+        store.close()
+        await remove(filename)
       }
     }
   })
 
+  benchmark.addCase('ExtraDiskCache (overwrite)', async () => {
+    const filename = await createTempFile()
+    const cache = await DiskCache.create(filename)
+    const view = new DiskCacheView(
+      cache
+    , new CacheIndexKeyConverter()
+    , new CacheJSONValueConverter()
+    )
+    for (let i = 100; i--;) {
+      view.set(i, i)
+    }
+
+    return {
+      iterate() {
+        for (let i = 100; i--;) {
+          view.set(i, i)
+        }
+      }
+    , async afterAll() {
+        cache.close()
+        await remove(filename)
+      }
+    }
+  })
+
+  benchmark.addCase('ExtraDiskStore (overwrite)', async () => {
+    const filename = await createTempFile()
+    const store = await DiskStore.create(filename)
+    const view = new DiskStoreView(
+      store
+    , new StoreIndexKeyConverter()
+    , new StoreJSONValueConverter()
+    )
+    for (let i = 100; i--;) {
+      view.set(i, i)
+    }
+
+    return {
+      iterate() {
+        for (let i = 100; i--;) {
+          view.set(i, i)
+        }
+      }
+    , async afterAll() {
+        store.close()
+        await remove(filename)
+      }
+    }
+  })
+
+
   benchmark.addCase('ExtraDiskCache (read)', async () => {
-    const cache = await DiskCache.create()
+    const filename = await createTempFile()
+    const cache = await DiskCache.create(filename)
     const view = new DiskCacheView(
       cache
     , new CacheIndexKeyConverter()
@@ -57,15 +130,22 @@ go(async () => {
       view.set(i, i)
     }
 
-    return () => {
-      for (let i = 1000; i--;) {
-        view.get(i)
+    return {
+      iterate() {
+        for (let i = 100; i--;) {
+          view.get(i)
+        }
+      }
+    , async afterAll() {
+        cache.close()
+        await remove(filename)
       }
     }
   })
 
   benchmark.addCase('ExtraDiskStore (read)', async () => {
-    const store = await DiskStore.create()
+    const filename = await createTempFile()
+    const store = await DiskStore.create(filename)
     const view = new DiskStoreView(
       store
     , new StoreIndexKeyConverter()
@@ -75,13 +155,20 @@ go(async () => {
       view.set(i, i)
     }
 
-    return () => {
-      for (let i = 1000; i--;) {
-        view.get(i)
+    return {
+      iterate() {
+        for (let i = 100; i--;) {
+          view.get(i)
+        }
+      }
+    , async afterAll() {
+        store.close()
+        await remove(filename)
       }
     }
   })
 
+  console.log(benchmark.name)
   for await (const result of benchmark.run()) {
     console.log(result)
   }
